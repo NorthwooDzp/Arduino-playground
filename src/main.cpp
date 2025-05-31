d0a2589fdf314f35322a6b4c358ffb6faf3beedf
Volodymyr L <northwoodzp@gmail.com>
2025-05-31 11:26:00 +0300

    added wifi connection and cerial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
 #include <Arduino.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
 #define LED_PIN 2
 #define SHORT_SIGNAL 200
 #define LONG_SIGNAL 500
+WebServer server(80);
+const char *ssid = "Xiaomi_76FA";
+const char *password = "gacty4df5";
 // put function declarations here:
 
-void setup() {
+void sosBlink();
+
+void setup()
+{
+  Serial.begin(115200);
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
+
+
+void loop()
+{
   // put your main code here, to run repeatedly:
+  sosBlink();
+}
+
+void sosBlink()
+{
   digitalWrite(LED_PIN, HIGH);
   delay(SHORT_SIGNAL);
   digitalWrite(LED_PIN, LOW);
@@ -50,6 +75,4 @@ void loop() {
   delay(SHORT_SIGNAL);
   digitalWrite(LED_PIN, LOW);
   delay(LONG_SIGNAL);
-
-
 }
